97089cb5d1037beeb571e3e1205aed5763fb4b67
klausDevTwo <klausic3@gmail.com>
2025-08-15 16:14:58 -0300

    add seasons and episodes for json converter.

--- src/main/java/br/com/thiagoDev/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/thiagoDev/screenmatch/ScreenmatchApplication.java
@@ -1,11 +1,16 @@
 package br.com.thiagoDev.screenmatch;
 
+import br.com.thiagoDev.screenmatch.model.DadosEpisodio;
 import br.com.thiagoDev.screenmatch.model.DadosSerie;
+import br.com.thiagoDev.screenmatch.model.DadosTemporada;
 import br.com.thiagoDev.screenmatch.services.ConsumoApi;
 import br.com.thiagoDev.screenmatch.services.ConverteDados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication  implements CommandLineRunner {
@@ -23,5 +28,17 @@ public class ScreenmatchApplication  implements CommandLineRunner {
         ConverteDados conversor = new  ConverteDados();
         DadosSerie dados = conversor.obterdados(json,DadosSerie.class);
         System.out.println(dados);
+        json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=eedb05eb");
+        DadosEpisodio dadosEpisodio = conversor.obterdados(json,DadosEpisodio.class);
+        System.out.println(dadosEpisodio);
+
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for (int i = 1; i <= dados.totalTemporadas(); i++){
+            json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=eedb05eb");
+            DadosTemporada dadosTemporada = conversor.obterdados(json,DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach(System.out::println);
     }
 }
